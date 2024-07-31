0c4a099dcca35e46ee38bbff30dd6d5369ea6f2e
mudit-51 <mudit.7.gupta+github@gmail.com>
2024-07-31 09:22:27 +0200

    added loading.tsx files

--- app/(authenticated)/profile/page.tsx
+++ app/(authenticated)/profile/page.tsx
@@ -1,8 +1,25 @@
-export default function Profile() {
+import { createClient } from "@/utils/supabase/server";
+import Image from "next/image";
+
+export default async function Profile() {
+  const supabase = createClient();
+  const user = (await supabase.auth.getUser()).data.user?.user_metadata;
   return (
-    <div className="container mx-auto p-4">
+    <div className="container flex flex-col mx-auto p-4">
       <h1 className="text-2xl font-bold">Profile Page</h1>
-      {/* Add your profile page content here */}
+      <div className="h-fit w-fit">
+        <Image
+          src={user?.avatar_url}
+          alt="Github user profile picture"
+          height={500}
+          width={500}
+          className="rounded-full "
+        />
+      </div>
+      <div>Welcome {user?.full_name}</div>
+      <div className=""> Email: {user?.email}</div>
+      <div className="">Skills: </div>
+      
     </div>
   );
 }
