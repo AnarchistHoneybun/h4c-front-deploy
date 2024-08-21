f956a6038ac2d06002f46de9b80e7e845fb77718
AnarchistHoneybun <thatonebipanda@gmail.com>
2024-08-21 10:30:41 +0530

    new footer

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,13 +1,34 @@
-import React from 'react'
+import React from 'react';
+import { FaFacebookF, FaTwitter, FaInstagram, FaYoutube, FaLinkedinIn } from 'react-icons/fa';
 import ModeToggle from './ModeToggle';
 
 const Footer: React.FC = () => {
   return (
-    <footer className="relative bg-background text-foreground p-8">
-      <div className="container mx-auto">
-        <div className="flex justify-start space-x-16">
+    <footer className="text-foreground py-16 relative" style={{ background: 'hsl(178, 74%, 9%)' }}>
+      <div className="container mx-auto px-20 sm:px-6 lg:px-8 flex justify-evenly">
+        <div className="flex flex-col items-center justify-center">
+          <div className="text-5xl font-black mb-4 text-green-500">SkillJourney</div>
+          <div className="flex space-x-4 justify-stretch text-3xl" style={{ color: 'hsl(125, 38%, 94%)' }}>
+            <a href="#" className="hover:text-orange-600">
+              <FaFacebookF />
+            </a>
+            <a href="#" className="hover:text-orange-600">
+              <FaTwitter />
+            </a>
+            <a href="#" className="hover:text-orange-600">
+              <FaInstagram />
+            </a>
+            <a href="#" className="hover:text-orange-600">
+              <FaYoutube />
+            </a>
+            <a href="#" className="hover:text-orange-600">
+              <FaLinkedinIn />
+            </a>
+          </div>
+        </div>
+        <div className="grid grid-cols-2 gap-8">
           <div>
-            <h3 className="text-lg font-bold mb-4">FAQ's</h3>
+            <h3 className="text-lg font-black mb-4" style={{ color: 'hsl(125, 38%, 94%)' }}>FAQ's</h3>
             <ul className="space-y-2">
               <li><a href="#" className="hover:underline">Link 1</a></li>
               <li><a href="#" className="hover:underline">Link 2</a></li>
@@ -15,28 +36,20 @@ const Footer: React.FC = () => {
             </ul>
           </div>
           <div>
-            <h3 className="text-lg font-bold mb-4">SOCIALS</h3>
+            <h3 className="text-lg font-black mb-4" style={{ color: 'hsl(125, 38%, 94%)' }}>More Info</h3>
             <ul className="space-y-2">
-              <li><a href="#" className="hover:underline">Twitter</a></li>
-              <li><a href="#" className="hover:underline">LinkedIn</a></li>
-              <li><a href="#" className="hover:underline">GitHub</a></li>
-            </ul>
-          </div>
-          <div>
-            <h3 className="text-lg font-bold mb-4">MORE INFO</h3>
-            <ul className="space-y-2">
-              <li><a href="#" className="hover:underline">About Us</a></li>
-              <li><a href="#" className="hover:underline">Contact</a></li>
               <li><a href="#" className="hover:underline">Privacy Policy</a></li>
+              <li><a href="#" className="hover:underline">About Us</a></li>
+              <li><a href="#" className="hover:underline">Contact Us</a></li>
             </ul>
           </div>
         </div>
       </div>
-      <div className="absolute bottom-0 right-0 p-4">
+      <div className="absolute bottom-4 right-4">
         <ModeToggle />
       </div>
     </footer>
-  )
-}
+  );
+};
 
-export default Footer
+export default Footer;
