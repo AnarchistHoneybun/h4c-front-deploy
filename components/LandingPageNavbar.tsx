db70fa533f3e87c203485ec5f951d5b118349618
mudit-51 <mudit.7.gupta+github@gmail.com>
2024-08-21 12:54:29 +0530

    navbar update

--- components/LandingPageNavbar.tsx
+++ components/LandingPageNavbar.tsx
@@ -1,20 +1,33 @@
 // components/LandingPageNavbar.tsx
-import React from 'react';
-import Link from 'next/link';
+
+import Link from "next/link";
+
+import { createClient } from "@/utils/supabase/client";
+import { Button } from "./ui/button";
+async function signInWithGithub() {
+  const supabase = createClient();
+  const { data, error } = await supabase.auth.signInWithOAuth({
+    provider: "github",
+    options: {
+      redirectTo: "http://localhost:3000/auth/callback",
+    },
+  });
+  if (error) {
+    console.log(error);
+  }
+}
 
 const LandingPageNavbar: React.FC = () => {
   return (
     <nav className="flex justify-between items-center bg-green-500 px-4 py-2">
-      <div className="text-white text-2xl font-bold">
-        SkillJourney
-      </div>
+      <div className="text-white text-2xl font-bold">SkillJourney</div>
       <div>
-        <Link href="/signup" className="bg-gray-800 text-white px-4 py-2 rounded-full mr-2">
-          Sign Up
-        </Link>
-        <Link href="/login" className="bg-white text-black px-4 py-2 rounded-full">
-          Login
-        </Link>
+        <Button
+          onClick={signInWithGithub}
+          className="bg-white text-black px-4 py-2 rounded-full"
+        >
+          Login with GitHub
+        </Button>
       </div>
     </nav>
   );
